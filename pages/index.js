78e049edc295b6270d77de578ae9f863c00431f9
YukiT1990 <07mikasanoyamani.idesitukikamo@gmail.com>
2021-05-07 07:26:20 +0900

    lesson 02

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1>Homepage</h1>
+    <div>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+    </div>
   )
 }
